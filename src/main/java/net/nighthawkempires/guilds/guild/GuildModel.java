17f160e7aace9b4b0426429d96bc792ea946c220
BuildTools <unconfigured@null.spigotmc.org>
2018-01-05 04:00:12 -0500

    re-add try/catch

--- src/main/java/net/nighthawkempires/guilds/guild/GuildModel.java
+++ src/main/java/net/nighthawkempires/guilds/guild/GuildModel.java
@@ -231,15 +231,19 @@ public class GuildModel extends AbstractPersistentModel<String> {
             }
         }
 
-        DataSection homeData = data.getSectionNullable("home");
-        if (homeData != null) {
-            World world = Bukkit.getWorld(homeData.getString("world"));
-            double x = homeData.getDouble("cord-x");
-            double y = homeData.getDouble("cord-y");
-            double z = homeData.getDouble("cord-z");
-            float yaw = Float.valueOf(homeData.getString("yaw"));
-            float pitch = Float.valueOf(homeData.getString("pitch"));
-            home = new Location(world, x, y, z, yaw, pitch);
+        try {
+            DataSection homeData = data.getSectionNullable("home");
+            if (homeData != null) {
+                World world = Bukkit.getWorld(homeData.getString("world"));
+                double x = homeData.getDouble("cord-x");
+                double y = homeData.getDouble("cord-y");
+                double z = homeData.getDouble("cord-z");
+                float yaw = Float.valueOf(homeData.getString("yaw"));
+                float pitch = Float.valueOf(homeData.getString("pitch"));
+                home = new Location(world, x, y, z, yaw, pitch);
+            }
+        } catch (NullPointerException e) {
+            home = null;
         }
     }
 
